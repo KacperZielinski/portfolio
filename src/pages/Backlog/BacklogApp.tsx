2353fcf72261ba526fe1c296c859dbf8262ffb99
Kacper Zielinski <kacper.zielinski@mailfence.com>
2022-01-23 20:56:07 +0100

    Testing for storing input fields in browser

--- src/pages/Backlog/BacklogApp.tsx
+++ src/pages/Backlog/BacklogApp.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 
 function BacklogApp() {
-    const [username, setUsername] = React.useState('')
-    const [password, setPassword] = React.useState('')
-
-      return (
+    return (
         <div>
             <p>Cześć, jestem BacklogApp!</p>
-            <Link to="/todo">Backlog</Link>
-            <form onSubmit={(e) => {
-                e.preventDefault();
-                console.log(username + password)
-            }}>
-                <input value={username} onChange={e => setUsername(e.target.value)} />
-                <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
-                <button type='submit'>SEND ME</button>
-            </form>
         </div>
-      );
+    )
 }
 
 export default BacklogApp;
--- src/pages/ForeignLanguageLearn/ForeignLanguageLearn.tsx
+++ src/pages/ForeignLanguageLearn/ForeignLanguageLearn.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function ForeignLanguageLearn() {
-  return (
-    <>
-      <p>Cześć, jestem LearnApp!</p>
-    </>
-  );
+    const [username, setUsername] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    return (
+        <div>
+            <p>Cześć, jestem BacklogApp!</p>
+            <Link to="/todo">Backlog</Link>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                console.log(username + password)
+            }}>
+                <input value={username} onChange={e => setUsername(e.target.value)} />
+                <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+                <button type='submit'>SEND ME</button>
+            </form>
+        </div>
+    );
 }
 
 export default ForeignLanguageLearn;
