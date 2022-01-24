2dd2dbafe49a68b73955781fdc6c03dee3a90daa
Kacper Zielinski <kacper.zielinski@mailfence.com>
2022-01-24 18:19:16 +0100

    Displaying list of todos

--- src/pages/Backlog/BacklogApp.tsx
+++ src/pages/Backlog/BacklogApp.tsx
@@ -1,22 +1,30 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { collection, getDocs } from "firebase/firestore";
 import {db} from "../firebase";
 
+interface SimpleTodo {
+    id: string;
+    title: string;
+}
+
 function BacklogApp() {
+    const [todos, setTodos] = useState<SimpleTodo[]>([]);
     useEffect(() => {
-        if(!!db) {
-            getDocs(collection(db!, "backlog"))
-                .then(res => {
-                    res.forEach((doc) => {
-                        console.log(`${doc.id} => ${doc.data().title}`);
-                    });
+        getDocs(collection(db!, "backlog"))
+            .then(res => {
+                const arr: SimpleTodo[] = [];
+                res.forEach((doc) => {
+                    console.log(`${doc.id} => ${doc.data().title}`);
+                    arr.push({id: doc.id, title: doc.data().title});
                 });
-        }
+                setTodos(arr);
+            });
     }, [])
 
     return (
         <div>
             <p>Cześć, jestem BacklogApp!</p>
+            {todos.map(todo => <div key={todo.id}>{todo.title}</div>)}
         </div>
     );
 }
