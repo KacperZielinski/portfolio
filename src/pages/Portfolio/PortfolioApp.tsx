1cd817de0f300371c01b994cb363f76f7c9d5cd1
Kacper Zielinski <kacper.zielinski@mailfence.com>
2022-02-03 20:11:15 +0100

    Initial HTML structure with some styling playground for portfolio

--- src/pages/Portfolio/PortfolioApp.tsx
+++ src/pages/Portfolio/PortfolioApp.tsx
@@ -2,11 +2,43 @@ import React from 'react';
 import './PortfolioApp.scss';
 
 function PortfolioApp() {
-  return (
-    <div>
-        Page under construction...
-    </div>
-  );
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>Logo</li>
+                </ul>
+                <ul>
+                    <li>Contact</li>
+                    <li>Linkedin</li>
+                    <li>Github</li>
+                    <li>Button pl</li>
+                    <li>Button en</li>
+                    <li>dark/light switch</li>
+                </ul>
+            </nav>
+            <main>
+                <article>
+                    <h1>Hi, I'm Kacper</h1>
+                    <section>
+                        <h3>Top skills</h3>
+                        <p>Java</p>
+                        <p>Spring</p>
+                        <p>React</p>
+                    </section>
+                    <section>
+                        <h3>Currently learning</h3>
+                        <p>Cloud</p>
+                        <p>DDD</p>
+                        <p>React</p>
+                    </section>
+                </article>
+            </main>
+            <footer>
+                <p>© Copyright 2022. All rights reversed.</p>
+            </footer>
+        </>
+    );
 }
 
 export default PortfolioApp;
